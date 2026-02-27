2f417f8ec9502e4797b8bbcd560638e6b1cc7abb
Mimikitty3 <85375536+Mimikitty3@users.noreply.github.com>
2026-02-26 21:17:47 -0300

    main.lua

---
+++ gamestate/main.lua
@@ -0,0 +1,27 @@
+gamestate = require("gamestate")
+
+function love.load()
+    -- gamestate:switch(newState) -- to switch to a new state
+
+    -- gamestate:reset() -- to reset the current state
+
+    -- gamestate:freeze() -- to freeze the current state
+
+    -- gamestate:unfreeze() -- to unfreeze the current state
+end
+
+function love.update(dt)
+    gamestate:update(dt)
+end
+
+function love.draw()
+    gamestate:draw()
+end 
+
+function love.keypressed(key)
+    gamestate:keypressed(key)
+end
+
+function love.keyreleased(key)
+    gamestate:keyreleased(key)
+end
